d73da89ee71d67d6df54e0a3bca134d63e4a5115
AkinaAoi <dungvutri25@gmail.com>
2026-02-16 15:22:46 +0700

    Update

--- sep-backend-java/core-service/src/main/java/com/sep/core_service/config/SecurityConfig.java
+++ sep-backend-java/core-service/src/main/java/com/sep/core_service/config/SecurityConfig.java
@@ -3,14 +3,22 @@ package com.sep.core_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Sử dụng BCrypt để băm mật khẩu
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) 
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); 
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
