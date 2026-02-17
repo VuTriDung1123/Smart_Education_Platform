e5459ac292db3f61cc872f504fed3f2d1908b4b8
AkinaAoi <dungvutri25@gmail.com>
2026-02-17 19:20:00 +0700

    Database: roles, permissions, user_roles, role_permissions, student_subject_grades, and academic_summaries

---
+++ sep-backend-java/core-service/src/main/java/com/sep/core_service/entity/Role.java
@@ -0,0 +1,37 @@
+package com.sep.core_service.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
+    // Tự động tạo bảng nối role_permissions
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "role_permissions",
+        joinColumns = @JoinColumn(name = "role_id"),
+        inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+}
